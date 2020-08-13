b63a13cee5df1b5436d13bb982cd5444b5ad729a
khillerb <64817143+khillerb@users.noreply.github.com>
2020-08-13 16:22:22 -0500

    JS update

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@
 let hitButton = document.getElementById('hitButton');
 let standButton = document.getElementById('standButton');
 let restartButton = document.getElementById('restartButton');
+let startButton = document.getElementById('startButton');
+let  betl = document.getElementById('betl');
+let  betlO = document.getElementById('betlO');
+let  betlOO = document.getElementById('betlOO');
+let  betSOO = document.getElementById('betSOO');
+let  enterBet = document.getElementById('enterBet');
 const winningMessageControl = document.getElementById('winningMessage');
+const startingMessageControl = document.getElementById('startingMessage');
 const winningText = document.querySelector('[winning-text]');
+const betData = document.querySelector('[bet-data]');
+
 let tempDealer = [];
 let tempPlayer = [];
 let tempPerson = [];
+let bet = 0;
+let bank = 2500;
 
 let deck = ['As','2s','3s','4s','5s','6s','7s','8s','9s','Ts','Js','Qs','Ks',
 				'Ah','2h','3h','4h','5h','6h','7h','8h','9h','Th','Jh','Qh','Kh',
@@ -28,15 +39,78 @@ const reset = () => {
 	tempPerson = [];
 	tempPlayer = [];
 	tempDealer = [];
-	turn = 0;
+	bet = 0;
+	bank = 2500;
+	startingMessageControl.classList.remove('show')
 	winningMessageControl.classList.remove('show')
-	//add/removeeventlisteners
+	hitButton.removeEventListener('click', pHit);
+	standButton.removeEventListener('click', pStand);
+	restartButton.removeEventListener('click', gamePlay);
+	startButton.removeEventListener('click', gamePlay)
+	hitButton.addEventListener('click', pHit);
+	standButton.addEventListener('click', pStand);
+	restartButton.addEventListener('click', gamePlay);
+	startButton.addEventListener('click', gamePlay)
+	betl.addEventListener('click', bet1);
+	betlO.addEventListener('click', bet10);
+	betlOO.addEventListener('click', bet100);
+	betSOO.addEventListener('click', bet500);
+	enterBet.addEventListener('click', enterBet);
+	shuffle()
+	
+	
+	
+}
+
+const bet1 = () => {
+	if ((bank-1) >= 0) {
+		bet += 1
+		bank -= 1
+		betData.innerText = bet
+	}
+	else {
+		alert("Not enough in the bank!")
+	}
+
+}
+const bet10 = () => {
+	if ((bank-10) >= 0) {
+		bet += 10
+		bank -= 10
+		betData.innerText = bet
+	}
+	else {
+		alert("Not enough in the bank!")
+	}
+
+}
+const bet100 = () => {
+	if ((bank-100) >= 0) {
+		bet += 100
+		bank -= 100
+		betData.innerText = bet
+	} else {
+		alert("Not enough in the bank!")
+	}
+} 
+const bet500 = () => {
+	if ((bank-500) >= 0) {
+		bet += 500
+		bank -= 500
+		betData.innerText = bet
+	} else {
+		alert("Not enough in the bank!")
+	}
+} 
+const enterBet = () => {
+	betl.removeEventListener('click', bet1)
+	betlO.removeEventListener('click', bet10)
+	betlOO.removeEventListener('click', bet100)
+	betSOO.removeEventListener('click', bet500)
+	deal()
 }
 
 const deal = () => {
-
-	reset()
-	shuffle()
 	player.push(deck.shift())
 	dealer.push(deck.shift())
 	player.push(deck.shift())
@@ -55,6 +129,7 @@ const shuffle = () => {
 	console.log(deck)
 }
 const dHit = () => {
+
 	dealer.push(deck.shift())
 	if (bustCheck(dealer)) {
 
@@ -71,21 +146,19 @@ const pHit = () => {
 	}
 }
 const pStand = () => {}
-const cardParse = (aElem,tempPerson) => {
-	if (aElem.charAt(0) == 'J' || aElem.charAt(0) == 'Q' || aElem.charAt(0) =='K'|| aElem.charAt(0) =='T') {
-		aElem = '10'
+const cardParse = (arrElem,temp) => {
+	if (arrElem.charAt(0) == 'J' || arrElem.charAt(0) == 'Q' || arrElem.charAt(0) =='K'|| arrElem.charAt(0) =='T') {
+		arrElem = '10'
 	}
-	if (aElem.charAt(0) == 'A') {
-		aElem = '11'
+	if (arrElem.charAt(0) == 'A') {
+		arrElem = '11'
 	}
-	aElem = aElem.replace(/\D+/g, "");
-	aElem = parseInt(aElem)
-	tempPerson.push(aElem)
+	arrElem = arrElem.replace(/\D+/g, "");
+	arrElem = parseInt(arrElem)
+	temp.push(arrElem)
 }
-const bustCheck = (person) => {
-	tempPerson = [];
-	person.forEach(element => cardParse(element,tempPerson));
-	tempPerson = tempPerson.reduce(function(accumulator, currentValue){return accumulator+currentValue});
+const bustCheck = (role) => {
+	tempPerson = make(role, tempPerson)
 	if (tempPerson > 21) {
 		return false
 	} 
@@ -95,23 +168,52 @@ const bustCheck = (person) => {
 
 }
 const setWinscreen = () => {
-	player.forEach(element => cardParse(element, tempPlayer));
-	tempPlayer = tempPlayer.reduce(function(accumulator, currentValue){return accumulator+currentValue});
-	dealer.forEach(element => cardParse(element, tempDealer));
-	tempDealer = tempDealer.reduce(function(accumulator, currentValue){return accumulator+currentValue});
+	tempPlayer = make(player,tempPlayer)
+	tempDealer = make(dealer,tempDealer)
 	if (!bustCheck(player)) {
-		winningText.innerText = 'You bust, Dealer wins!'
+		winningText.innerText = `You bust and lose ${'$' + bet}, Dealer wins!`
+		bet = 0
 	} else if (!bustCheck(dealer)) {
-		winningText.innerText = 'Dealer busts, You win!'
+		winningText.innerText = `Dealer busts, You win ${'$'+ 2*bet}!`
+		bank += 2*bet
 	} else if (tempDealer == tempPlayer) {
-		winningText.innerText = 'Push/Tie! Restart!'
+		winningText.innerText = 'Push. Bet returned'
+		bank += bet
 	} else {
-		winningText.innerText = `${(tempDealer > tempPlayer) ? "Dealer" : "Player"} wins!`
+		winningText.innerText = `${(tempDealer > tempPlayer) ? `Dealer Wins! You lose ${'$' + bet}` : `You win ${'$'+ 2*bet}!`}`
+		if (tempDealer > tempPlayer){
+			bet = 0
+		} else {
+			bank += 2*bet
+		}
 	}
 	winningMessageControl.classList.add('show')
 }
-
+const make = (role,temp) => {
+	role.forEach(element => cardParse(element,temp));
+	temp = temp.reduce(function(accumulator, currentValue){return accumulator+currentValue});
+	return temp
+}
+betl.addEventListener('click', bet1);
+betlO.addEventListener('click', bet10);
+betlOO.addEventListener('click', bet100);
+betSOO.addEventListener('click', bet500);
+enterBet.addEventListener('click', enterBet);
 hitButton.addEventListener('click', pHit);
 standButton.addEventListener('click', pStand);
-restartButton.addEventListener('click', deal);
-deal()
+restartButton.addEventListener('click', gamePlay);
+startButton.addEventListener('click', gamePlay);
+
+const gamePlay = () => {
+	reset()
+	
+
+
+	//wait for event listener for bets here.
+	
+
+
+
+}
+
+
